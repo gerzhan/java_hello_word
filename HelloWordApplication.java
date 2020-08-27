6fe56dd09bf7c4f593571c8e4f456a3df89a593a
Nikolay Gerzhan <nikolay.gerzhan@gmail.com>
2020-08-27 18:02:58 +0700

    feat: show ten numbers

--- HelloWordApplication.java
+++ HelloWordApplication.java
@@ -2,5 +2,8 @@ public class HelloWordApplication {
 
     public static void main(String[] args) {
         System.out.println("Hello Word!");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
     }
 }
