13a486a76b3020f29e2e33186f17945132f3b19e
Nikolay Gerzhan <nikolay.gerzhan@gmail.com>
2020-08-30 14:54:16 +0700

    chore(HelloWordApplication): display List<Foo>

--- HelloWordApplication.java
+++ HelloWordApplication.java
@@ -1,39 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Foo {
   int id;
   String pp;
   String name;
+
+  public String display() {
+    return "Foo: id=" + this.id + ", name=" + this.name + ", pp=" + this.pp;
+  }
 }
 
 // NOTE: public перед классом требовалось указывать в ранних версиях Java
 class HelloWordApplication {
 
-  static void displayItem(Foo item) {
-    System.out.println(
-      "Foo: id=" + item.id + ", name=" + item.name + ", pp=" + item.pp
-    );
-  }
-
   // NOTE: обязательный метод для запуска приложения
   public static void main(String[] args) {
     System.out.println("Hello Word!!");
 
+    List<Foo> list = new ArrayList();
     Foo item1 = new Foo();
     item1.id = 1;
     item1.name = "one";
-    HelloWordApplication.displayItem(item1);
+    list.add(item1);
 
     Foo item2 = new Foo();
     item2.id = 2;
     item2.name = "two";
+    list.add(item2);
 
-    HelloWordApplication.displayItem(item2);
     Foo item3 = new Foo();
     item3.id = 3;
     item3.name = "three";
-    HelloWordApplication.displayItem(item3);
+    list.add(item3);
 
-    for (int i = 1; i <= 5; i++) {
-      System.out.println(i);
+    for (int i = 0; i < list.size(); i++) {
+      System.out.println(list.get(i).display());
     }
   }
 }
